1dde9b12443d7bb03b23584291d9db3d3960b583
Michael Koval <mkoval@cs.cmu.edu>
2014-08-19 14:37:57 -0400

    Switched to openrave_catkin.

--- catkin.cmake
+++ catkin.cmake
@@ -1,26 +1,25 @@
 cmake_minimum_required(VERSION 2.8.3)
 
-find_package(catkin REQUIRED cmake_modules)
+find_package(catkin REQUIRED cmake_modules openrave_catkin)
 catkin_package(
     INCLUDE_DIRS include/
     LIBRARIES ${PROJECT_NAME}
-    DEPENDS ompl openrave
+    DEPENDS ompl
 )
 
 find_package(Boost REQUIRED COMPONENTS system)
-find_package(OpenRAVE REQUIRED)
 find_package(OMPL REQUIRED)
 find_package(TinyXML REQUIRED)
 
 include_directories(
     include/${PROJECT_NAME}
-    ${OpenRAVE_INCLUDE_DIRS}
     ${OMPL_INCLUDE_DIRS}
     ${TinyXML_INCLUDE_DIRS}
+    ${catkin_INCLUDE_DIRS}
 )
 link_directories(
-    ${OpenRAVE_LIBRARY_DIRS}
     ${OMPL_LIBRARY_DIRS}
+    ${catkin_LIBRARY_DIRS}
 )
 
 # Generate the OMPL planner wrappers.
@@ -53,29 +52,21 @@ target_link_libraries(${PROJECT_NAME}
     ${Boost_LIBRARIES}
     ${TinyXML_LIBRARIES}
 )
-set_target_properties(${PROJECT_NAME} PROPERTIES
-    COMPILE_FLAGS "${OpenRAVE_CXX_FLAGS} ${OMPL_CXX_FLAGS}"
-    LINK_FLAGS "${OpenRAVE_LINK_FLAGS} ${OMPL_LINK_FLAGS}"
-)
 
 # OpenRAVE plugin.
-add_library(${PROJECT_NAME}_plugin SHARED
-    src/OMPLMain.cpp)
-target_link_libraries(${PROJECT_NAME}_plugin
-    ${PROJECT_NAME}
-    ${OpenRAVE_LIBRARIES}
-    ${Boost_LIBRARIES}
+openrave_plugin("${PROJECT_NAME}_plugin"
+    src/OMPLMain.cpp
 )
-set_target_properties(${PROJECT_NAME}_plugin PROPERTIES
-    PREFIX ""
-    COMPILE_FLAGS "${OpenRAVE_CXX_FLAGS}"
-    LINK_FLAGS "${OpenRAVE_LINK_FLAGS}"
-    LIBRARY_OUTPUT_DIRECTORY "${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_LIB_DESTINATION}/openrave-${OpenRAVE_LIBRARY_SUFFIX}")
+target_link_libraries("${PROJECT_NAME}_plugin"
+    ${PROJECT_NAME}
+    ${Boost_LIBRARIES}
+    ${catkin_LIBRARIES}
+)
 
 install(TARGETS or_ompl
-    LIBRARY DESTINATION "${CATKIN_PACKAGE_LIB_DESTINATION}")
-install(TARGETS or_ompl_plugin
-    LIBRARY DESTINATION "${CATKIN_PACKAGE_LIB_DESTINATION}/openrave-${OpenRAVE_LIBRARY_SUFFIX}")
+    LIBRARY DESTINATION "${CATKIN_PACKAGE_LIB_DESTINATION}"
+)
 install(DIRECTORY "include/${PROJECT_NAME}/"
     DESTINATION "${CATKIN_PACKAGE_INCLUDE_DESTINATION}"
-    PATTERN ".svn" EXCLUDE)
+    PATTERN ".svn" EXCLUDE
+)
