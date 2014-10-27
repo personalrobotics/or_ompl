2c91100a107ee453ac0d117147e9fd320a911ff4
Mike Koval <mkoval@cs.cmu.edu>
2014-10-27 16:49:41 -0400

    Added licensing information.

--- src/OMPLSimplifer.h
+++ src/OMPLSimplifer.h
@@ -3,6 +3,8 @@
 Copyright (c) 2014, Carnegie Mellon University
 All rights reserved.
 
+Authors: Michael Koval <mkoval@cs.cmu.edu>
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
